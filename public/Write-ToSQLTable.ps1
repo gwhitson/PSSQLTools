33ba93c7bac61d59d1500308c0f9d6deb11512a0
gavin <rockywhitson4@gmail.com>
2024-01-05 11:01:56 -0600

    update

--- public/Write-ToSQLTable.ps1
+++ public/Write-ToSQLTable.ps1
@@ -2,20 +2,59 @@ function Write-ToSQLTable {
     <#
     
         .SYNOPSIS
-        Write-ToSQLTable 
+        Write-ToSQLTable takes in a hashmap as well as 3 additional strings and one
+        other parameter for specifying the server. The hashmap keys must be the same
+        as the columns in the table you are attempting to write to
 
         .DESCRIPTION
+        InsertObject object can be given as a parameter or passed into function, then
+        one of two parameter sets are used. If you intend to use a connection object
+        multiple times, you can pass a [System.Data.SqlClient.SqlConnection] object in
+        as the -Connection parameter. Otherwise a Server name is needed to create the 
+        connection object in the function. Table, Schema, and Database are needed in
+        any calls as they are used to build the SQL commands being ran
         
-        .PARAMETER InsertKeys
-        .PARAMETER InsertValues
+        .PARAMETER InsertObject
+        Input hashmap, meant to correspond to a row of the database. An object being
+        passed in should have keys and values that correspond to the columns and 
+        data types being stored in the table. If the function finds a mismatch it throws 
+        an error
+
         .PARAMETER Table
+        String parameter that specified the SQL table being accessed. Used to build 
+        the SQL Commands
+
         .PARAMETER Schema
-        .PARAMETER Server
+        String parameter that specified the SQL schema being accessed. Used to build 
+        the SQL Commands
+
         .PARAMETER Database
+        String parameter that specified the SQL Database being accessed. Used to build 
+        the SQL Commands
+
+        .PARAMETER Server
+        String parameter that specified the SQL Server being accessed. Used to build 
+        the SQL Commands
+
         .PARAMETER Conn
+        [System.Data.SqlClient.SqlConnection] object that is used to execute SQL commands
+        on the server. A connection object that is passed in is returned to the state it
+        was in when passed in.
     
         .EXAMPLE
+        Write-ToSQLTable 
+            -InsertObject @{col1=1;col2="two";col3=(get-date)} 
+            -Table "Test-RunInfo" 
+            -Database "ScriptTesting" 
+            -Server "SQLSERVER" 
+            -Verbose
     
+        Write-ToSQLTable 
+            -InsertObject @{col1=1;col2="two";col3=(get-date)} 
+            -Table "Test-RunInfo" 
+            -Database "ScriptTesting" 
+            -Connection $conn
+            -Verbose
     #>
     [CmdletBinding(DefaultParameterSetName = 'CreateConn')]
     param(
