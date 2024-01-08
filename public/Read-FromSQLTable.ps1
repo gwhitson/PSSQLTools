fb4d65fd871b1f8ccc60f7ad0a171eadfe5a8e11
gavin <rockywhitson4@gmail.com>
2024-01-08 13:45:01 -0600

    update

--- public/Read-FromSQLTable.ps1
+++ public/Read-FromSQLTable.ps1
@@ -2,79 +2,140 @@ function Read-FromSQLTable {
     <#
     
         .SYNOPSIS
-        
+
         .DESCRIPTION
         
-        .PARAMETER filepath
-    
+        .PARAMETER InsertObject
+        .PARAMETER Table
+        .PARAMETER Schema
+        .PARAMETER Database
+        .PARAMETER Server
+        .PARAMETER Conn
         .EXAMPLE
-    
     #>
+    [CmdletBinding(DefaultParameterSetName = 'CreateConn','SelectAll')]
     param(
-        [Parameter(Mandatory=$true, position=0)]
-        [String]$database,
-        [Parameter(Mandatory=$true, position=1)]
-        [String]$table = "master",
-        [Parameter(Mandatory=$false, position=2)]
-        [String]$NumRows = "50",
-        [Parameter(Mandatory=$false, position=3)]
+        [Parameter(Mandatory, ParameterSetName='SelectSome', position=0, ValueFromPipeline)]
+        [hashtable]$SearchObject,
+        [Parameter(Mandatory, ParameterSetName='SelectAll', position=0)]
+        [switch]$SelectAll,
+        [Parameter(ParameterSetName="SelectSome", HelpMessage="Default Value is `'TOP`'")]
+        [ValidateSet("TOP", "DISTINCT")]
+        [String]$SelectModifier = "TOP",
+        [Parameter(ParameterSetName="SelectSome", HelpMessage="Default Value is `'DESCENDING`'")]
+        [ValidateSet("ASCENDING", "DESCENDING")]
+        [String]$Order = "DESCENDING",
+        [Parameter(ParameterSetName="SelectSome", HelpMessage="Default Value is first key in Search Object")]
+        [String]$OrderBy = $($SearchObject.keys)[0],
+        [Parameter(ParameterSetName="SelectSome", HelpMessage="Default Value is 1")]
+        [String]$NumRows = "1",
+        [Parameter(ParameterSetName='CreateConn')]
+        [Parameter(ParameterSetName='PassedConn')]
+        [String]$Table = "master",
+        [Parameter(ParameterSetName='CreateConn')]
+        [Parameter(ParameterSetName='PassedConn')]
         [String]$Schema = "dbo",
-        [Parameter(Mandatory=$false, position=4)]
-        [String]$server,
-        [Parameter(Mandatory=$false, position=5)]
-        [Array]$values = ('*'),
-        [Parameter(Mandatory=$false)]
-        [ValidateSet("Top", "Distinct")]
-        [String]$SelectModifier,
-        [Parameter(ParameterSetName='Order',Mandatory=$false)]
-        [ValidateSet("Ascending", "Descending")]
-        [String]$Order,
-        [Parameter(ParameterSetName='Order',Mandatory=$false)]
-        [String]$OrderBy = "",
-        [Parameter(Mandatory=$false, ValueFromPipeline)]
-        [System.Data.SqlClient.SqlConnection]$conn = $null
+        [Parameter(Mandatory, ParameterSetName='CreateConn', position=2)]
+        [Parameter(Mandatory, ParameterSetName='PassedConn', position=2)]
+        [String]$Database,
+        [Parameter(Mandatory, ParameterSetName='CreateConn', position=3)]
+        [String]$Server,
+        [Parameter(Mandatory, ParameterSetName='PassedConn', position=3)]
+        [System.Data.SqlClient.SqlConnection]$Connection = $null
     )
 
-    ############# INITIALIZE OBJECTS ######################################
-    if ($conn -eq $null){
-        $conn = New-Object System.Data.SqlClient.SqlConnection
-        $conn.ConnectionString = "Server = $server; Database = $database; Integrated Security = True"
-        try{
-            $conn.open()
-        } catch [System.Data.SqlClient.SqlException]{
-            write-error "database not accessible to user account"
-            break
+    begin{
+        if ($PSCmdlet.ParameterSetName -eq "CreateConn"){
+            $conn = New-Object System.Data.SqlClient.SqlConnection
+                $conn.ConnectionString = "Server = $($server); Database = $($database); Integrated Security = True"
+                try{
+                    $conn.open()
+                } catch [System.Data.SqlClient.SqlException]{
+                    write-error "database not accessible to user account"
+                        return $null
+                }
+        } else {
+            $passedConnState = $conn.State
+                if ($passedConnState -eq "Closed"){
+                    $conn.Open()
+                }
         }
-    }
 
-    if ($conn.State -ne "Open"){
-        $conn.Open()
-    }
+        $query = New-Object System.Data.SqlClient.SqlCommand
+        $query.connection = $conn
 
-    $query = New-Object System.Data.SqlClient.SqlCommand
-    $query.connection = $conn
+        $adapter = New-Object System.Data.SqlClient.SqlDataAdapter
+        $ds = New-Object System.Data.DataSet
 
-    $adapter = New-Object System.Data.SqlClient.SqlDataAdapter
-    $ds = New-Object System.Data.DataSet
+        $query.CommandText = "SELECT * FROM [$($database)].[$($schema)].[$($table)];"
+        $adapter.SelectCommand = $query
+        $adapter.fill($ds)
 
-    try {$SelectModifier = $SelectModifier.toUpper()} catch {}
-    try {$Order = $Order.ToUpper()} catch {}
-    $ValuesFormatted = ""
+        $SearchObject.Keys | ForEach-Object {
+            if ($_ -notin $ds.Tables.Columns.ColumnName){
+                Throw "Key in Search Object hashtable does not correspond to column of selected table"
+                return 0;
+            }
+        }
+    } process {
+        $queryText = ""
+        $KeysFormatted = ""
+        $counter = 0
+        
+        $SearchObject.Keys | ForEach-Object {
+            $KeysFormatted += (ConvertTo-SQLColumnName $($_))
+            $counter += 1
 
-    ############# VERIFY INPUT VALUE TYPES ################################
+            if ($counter -lt $SearchObject.Count){
+                $KeysFormatted += ", "
+            }
+        }
 
-    if ($values.Count -eq 1 -and $values[0] -eq "*"){
-        $ValuesFormatted = "*"
-    } else {
-        $values | % {
-            $ValuesFormatted += ConvertTo-SQLColumnName $_
-            if ($_ -ne $values[-1]){
-                $ValuesFormatted += ", "
+        $queryText = "SELECT "
+
+            if ($SelectModifier -eq "TOP" -or $SelectModifier -eq "DISTINCT"){
+                $queryText += ("$SelectModifier ($NumRows) ")
+            } else {
+                if ($NumRows -gt 0){
+                    $queryText += ("TOP ($NumRows) ")
+                }
+            }
+
+        $queryText += [String]($ValuesFormatted + " FROM [$database].[$schema].[$table]")
+
+            if ($Order -in @("ASCENDING", "DESCENDING")){
+                $queryText += " ORDER BY "
+                    if ($OrderBy -in $values -or $values -eq @('*')){
+                        if ($OrderBy -eq ""){
+                            Write-Error "Must Specify value to order by"
+                                break
+                        }
+                        $queryText += $OrderBy
+                            if ($Order -eq "ASCENDING"){
+                                $queryText += " ASC"
+                            } else {
+                                $queryText += " DESC"
+                            }
+                    } else {
+                        write-error "Key to order by must be included in keys to select"
+                            break
+                    }
+            }
+
+        $queryText += ";"
+
+            write-Verbose $queryText
+    } end {
+        if ($PSCmdlet.ParameterSetName -eq "CreateConn"){
+            $conn.close()
+        } else {
+            if ($passedConnState -ne "Closed"){
+                $conn.close()
             }
         }
     }
-
-    ############# QUERY BUILDER ###########################################
+}
+function Read-FromSQLTable {
 
     $queryText = "SELECT "
 
